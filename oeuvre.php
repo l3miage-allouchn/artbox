b57f17972196b34563d627c47adb506ecb87d710
l3miage-allouchn <158454979+l3miage-allouchn@users.noreply.github.com>
2026-08-05 12:06:49 +0200

    Recuperation d'une oeuvre depuis la base de donnees sur la page de detail

--- oeuvre.php
+++ oeuvre.php
@@ -1,26 +1,22 @@
 <?php
     require 'header.php';
-    require 'oeuvres.php';
+    require 'bdd.php';
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
         header('Location: index.php');
+        exit;
     }
 
-    $oeuvre = null;
+    $pdo = connexion();
+    $stmt = $pdo->prepare('SELECT * FROM oeuvres WHERE id = ?');
+    $stmt->execute([intval($_GET['id'])]);
+    $oeuvre = $stmt->fetch();
 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
-
-    // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
+    // Si aucune oeuvre trouvée, on redirige vers la page d'accueil
+    if($oeuvre === false) {
         header('Location: index.php');
+        exit;
     }
 ?>
 
